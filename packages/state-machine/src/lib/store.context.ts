d95a843eb8b6c88283b4f063b6025970346519b0
PowWoWJs <js.developer.powwow@gmail.com>
2025-02-23 16:58:05 +0100

    feat(stateMachine): Add contexts reactivity

--- packages/state-machine/src/lib/store.context.ts
+++ packages/state-machine/src/lib/store.context.ts
@@ -1,7 +1,11 @@
 import { RecordKey } from '@powwow-js/nullable';
+import { EventEmitter, IEventEmitter } from '@powwow-js/emitter';
 
-export class StoreContext<T extends Record<RecordKey, unknown>> {
+export class StoreContext<T extends Record<RecordKey, unknown>>
+  implements Pick<IEventEmitter<{ changed: T }>, 'on' | 'off'>
+{
   private context;
+  private emitter = new EventEmitter<{ changed: T }>();
 
   constructor(initialValue: T) {
     this.context = initialValue;
@@ -12,14 +16,24 @@ export class StoreContext<T extends Record<RecordKey, unknown>> {
   public set<K extends keyof T>(updater: Pick<T, K> | undefined | ((ctx: T) => Pick<T, K>)): typeof this {
     if (typeof updater === 'function') {
       this.context = { ...this.context, ...updater(this.context) };
+      this.emitter.emit('changed', this.context);
     }
     if (updater && typeof updater === 'object') {
       this.context = { ...this.context, ...updater };
+      this.emitter.emit('changed', this.context);
     }
     return this;
   }
 
   public get() {
     return this.context;
+  }
+
+  public on<P extends Parameters<typeof this.emitter.on>>(...params: P): void {
+    return this.emitter.on.apply(this, params);
+  }
+
+  public off<P extends Parameters<typeof this.emitter.off>>(...params: P): void {
+    return this.emitter.off.apply(this, params);
   }
 }
--- packages/state-machine/src/lib/value.context.ts
+++ packages/state-machine/src/lib/value.context.ts
@@ -1,15 +1,29 @@
-export class ValueContext<T> {
+import { EventEmitter, IEventEmitter } from '@powwow-js/emitter';
+
+export class ValueContext<T> implements Pick<IEventEmitter<{ changed: T }>, 'on' | 'off'> {
   private contextValue;
+  private emitter = new EventEmitter<{ changed: T }>();
 
   constructor(initialValue: T) {
     this.contextValue = initialValue;
   }
 
   public set value(newValue: T) {
-    this.contextValue = newValue;
+    if (!Object.is(newValue, this.contextValue)) {
+      this.contextValue = newValue;
+      this.emitter.emit('changed', this.contextValue);
+    }
   }
 
   public get value() {
     return this.contextValue;
+  }
+
+  public on<P extends Parameters<typeof this.emitter.on>>(...params: P): void {
+    return this.emitter.on.apply(this, params);
+  }
+
+  public off<P extends Parameters<typeof this.emitter.off>>(...params: P): void {
+    return this.emitter.off.apply(this, params);
   }
 }
